bc584270f9f7ae07c206c7b1a7cef236fdbb91e6
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-07-01 09:41:15 +0200

    remove stats

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ import sand_common from './src/shaders/sand/common.glsl'
 // __gui_import__
 
 // const stats = new Stats()
-stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom)
+// stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+// document.body.appendChild(stats.dom)
 
 const textureLoader = new THREE.TextureLoader()
 
